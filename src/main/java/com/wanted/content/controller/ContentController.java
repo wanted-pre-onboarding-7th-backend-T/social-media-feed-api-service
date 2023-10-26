20d7ab985094e7255d73a3f48c3de3d660d208ef
Daeun Kim <jkde7721@naver.com>
2023-10-26 23:42:02 +0900

    feat: 게시물 좋아요 ContentController 구현 #3
    - 경로 변수로 게시물 식별자를 전달받아 Service에게 처리 요청
    - 결과 좋아요 개수를 반환받아 게시물 식별자와 함께 응답으로 전달

---
+++ src/main/java/com/wanted/content/controller/ContentController.java
@@ -0,0 +1,26 @@
+package com.wanted.content.controller;
+
+import com.wanted.common.dto.ResponseDto;
+import com.wanted.content.dto.ContentLikeResponseDto;
+import com.wanted.content.service.ContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/contents")
+public class ContentController {
+
+    private final ContentService contentService;
+
+    @PostMapping("/{contentId}/likes")
+    public ResponseDto<ContentLikeResponseDto> increaseLikeCount(@PathVariable Long contentId) {
+        Long likeCount = contentService.increaseLikeCount(contentId);
+        return new ResponseDto<>(200, HttpStatus.OK.name(),
+            new ContentLikeResponseDto(contentId, likeCount));
+    }
+}
