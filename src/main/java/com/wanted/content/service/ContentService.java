816389f2e831d20f571b5e9820faef6555a5d4de
kho903 <gmldnr2222@naver.com>
2023-10-26 21:21:40 +0900

    feat: 게시물 상세 조회 in ContentService #2 - findContentDetails(Long id) - 조회 수 증가 로직 구현 - 존재하지 않는 id일 경우 CommonException 에서 NOT_FOUND 던짐

---
+++ src/main/java/com/wanted/content/service/ContentService.java
@@ -0,0 +1,31 @@
+package com.wanted.content.service;
+
+import com.wanted.common.dto.ResponseDto;
+import com.wanted.common.exception.CommonException;
+import com.wanted.content.dto.ContentsDetailsResponseDto;
+import com.wanted.content.entity.Content;
+import com.wanted.content.repository.ContentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ContentService {
+
+    private final ContentRepository contentRepository;
+
+    @Transactional
+    public ResponseDto<ContentsDetailsResponseDto> findContentDetails(Long id) {
+        if (contentRepository.findById(id).isPresent()) {
+            contentRepository.increasingViewCount(id);
+            Content content = contentRepository.findById(id).get();
+            ContentsDetailsResponseDto contentsDetailsResponseDto = ContentsDetailsResponseDto.toDto(content);
+            return new ResponseDto<>(200, "성공적으로 게시물을 가져왔습니다.", contentsDetailsResponseDto);
+        } else {
+            throw new CommonException(HttpStatus.NOT_FOUND, "해당 게시물이 존재하지 않습니다..");
+        }
+    }
+
+}
