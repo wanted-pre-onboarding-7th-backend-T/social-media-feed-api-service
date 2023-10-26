3b2e8eff3d2a4cb0c393e6ccb118d7c6a1750e99
Siglee2247 <lgh941107@gmail.com>
2023-10-26 21:15:00 +0900

    feat: passwordEncoder Bean 등록 #1 암호화를 위한 passwordEncoder 빈 등록

--- src/main/java/com/wanted/common/config/SecurityConfig.java
+++ src/main/java/com/wanted/common/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.wanted.common.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,4 +20,8 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
