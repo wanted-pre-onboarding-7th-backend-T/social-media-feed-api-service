a9172fdfee1644e26f73b70eade2db414cdcc357
kho903 <gmldnr2222@naver.com>
2023-10-26 21:19:09 +0900

    feat: 게시물 상세 조회를 위한 Response DTO #2
    - TODO: 현재 toDto() 메서드 포함 - 추후 리펙토링

---
+++ src/main/java/com/wanted/content/dto/ContentsDetailsResponseDto.java
@@ -0,0 +1,63 @@
+package com.wanted.content.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wanted.content.entity.Content;
+import com.wanted.content.enums.SnsType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ContentsDetailsResponseDto {
+
+    @JsonProperty("contents_id")
+    private Long id;
+
+    @JsonProperty("content_sns_id")
+    private String contentSnsId;
+
+    @Enumerated(EnumType.STRING)
+    @JsonProperty("type")
+    private SnsType type;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("content")
+    private String content;
+
+    @JsonProperty("view_count")
+    private Long viewCount;
+
+    @JsonProperty("like_count")
+    private Long likeCount;
+
+    @JsonProperty("share_count")
+    private Long shareCount;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    public static ContentsDetailsResponseDto toDto(Content content) {
+        return ContentsDetailsResponseDto.builder()
+            .id(content.getId())
+            .contentSnsId(content.getContentSnsId())
+            .type(content.getType())
+            .title(content.getTitle())
+            .content(content.getContent())
+            .viewCount(content.getViewCount())
+            .likeCount(content.getLikeCount())
+            .shareCount(content.getShareCount())
+            .updatedAt(content.getUpdatedAt())
+            .createdAt(content.getCreatedAt())
+            .build();
+    }
+}
