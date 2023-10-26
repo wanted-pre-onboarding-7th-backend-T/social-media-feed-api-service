28ae041411ab9aa50108314d6457ad4db3dc52ce
ffolabear <ffolabear@gmail.com>
2023-10-26 11:58:48 +0900

    init: 프로젝트 초기 설정

--- src/main/java/com/wanted/PreOnboardingApplication.java
+++ src/main/java/com/wanted/PreOnboardingApplication.java
@@ -2,12 +2,12 @@ package com.wanted;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PreOnboardingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PreOnboardingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PreOnboardingApplication.class, args);
+    }
 }
