4cf3f6b8981f287bb4d41e853af246e7f7afd4ef
Daeun Kim <jkde7721@naver.com>
2023-10-27 01:52:14 +0900

    feat: 각 SNS 별 API 호출용 정보에 대한 팩토리 클래스 구현 #3
    - 해당 게시물의 타입에 따라 분기하여, 주입된 SNS API 정보들 중 하나를 반환
    - 지원하지 않는 SNS의 게시물일 경우에는 CommonException 예외 발생

---
+++ src/main/java/com/wanted/external/info/SnsApiInfoFactory.java
@@ -0,0 +1,34 @@
+package com.wanted.external.info;
+
+import com.wanted.common.exception.CommonException;
+import com.wanted.content.enums.SnsType;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class SnsApiInfoFactory {
+
+    private final InstagramApiInfo instagramApiInfo;
+    private final TwitterApiInfo twitterApiInfo;
+    private final FacebookApiInfo facebookApiInfo;
+    private final ThreadApiInfo threadApiInfo;
+
+    public SnsApiInfo getSnsApiInfo(SnsType type) {
+        if (type == SnsType.INSTAGRAM) {
+            return instagramApiInfo;
+        }
+        if (type == SnsType.TWITTER) {
+            return twitterApiInfo;
+        }
+        if (type == SnsType.FACEBOOK) {
+            return facebookApiInfo;
+        }
+        if (type == SnsType.THREAD) {
+            return threadApiInfo;
+        }
+        throw new CommonException(HttpStatus.BAD_REQUEST, "지원하지 않는 SNS 입니다.");
+    }
+}
