478b6f3dacc743e1d343c947c49659c8c44e8758
Siglee2247 <lgh941107@gmail.com>
2023-10-28 00:04:40 +0900

    feat: 로그아웃 성공 Handler 구현 #1 - 로그아웃 시 redis에서 refreshtoken 삭제 및 - cookie 안의 refresh 만료

---
+++ src/main/java/com/wanted/common/security/handler/LogoutSuccessCustomHandler.java
@@ -0,0 +1,56 @@
+package com.wanted.common.security.handler;
+
+import com.wanted.common.redis.repository.RedisRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LogoutSuccessCustomHandler implements LogoutSuccessHandler {
+    private final RedisRepository repository;
+
+    private static void createResponse(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.OK.value());
+        response.getWriter().flush();
+    }
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException {
+        Cookie[] cookies = validCookiesExist(request);
+
+        deleteCookie(searchCookieProperties(cookies));
+        deleteTokenToRedis(searchCookieProperties(cookies));
+
+        createResponse(response);
+    }
+
+    private void deleteTokenToRedis(Cookie refreshCookie) {
+        String refreshToken = refreshCookie.getValue();
+        repository.delete(refreshToken);
+    }
+
+    private Cookie[] validCookiesExist(HttpServletRequest request) {
+        return Optional.ofNullable(request.getCookies()).orElse(new Cookie[]{});
+    }
+
+    private Cookie searchCookieProperties(Cookie[] cookies) {
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals("Refresh"))
+                .findFirst()
+                .orElse(new Cookie("Refresh",""));
+    }
+
+    private void deleteCookie(Cookie cookie) {
+        cookie.setMaxAge(0);
+    }
+}
