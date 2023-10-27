6b8315af51e56f64c46c196559d3e879f35e00db
Siglee2247 <lgh941107@gmail.com>
2023-10-28 01:19:30 +0900

    style: 불필요한 공백 제거 #1

--- src/main/java/com/wanted/common/security/enums/AuthExceptionCode.java
+++ src/main/java/com/wanted/common/security/enums/AuthExceptionCode.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum AuthExceptionCode {
     ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, " Access Token 만료"),
     INVALID_SIGNATURE_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED,"서명이 올바르지 않습니다"),
-
-    UNAUTHENTICATED(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다.");
+    UNAUTHENTICATED(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다.")
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
