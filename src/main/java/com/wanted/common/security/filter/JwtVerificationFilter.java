1c603760ddc81d586e243d392e86b6ba34df43a3
Siglee2247 <lgh941107@gmail.com>
2023-10-27 23:25:19 +0900

    feat: 인가를 위한 VerificationFilter 구현 #1 - 인가를 위한 VerificationFilter 구현 - OncePerRequestFilter 구현을 통한 Filter 구현

---
+++ src/main/java/com/wanted/common/security/filter/JwtVerificationFilter.java
@@ -0,0 +1,76 @@
+package com.wanted.common.security.filter;
+
+import com.wanted.common.security.enums.AuthExceptionCode;
+import com.wanted.common.security.utils.JwtProvider;
+import com.wanted.common.security.vo.Principal;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtVerificationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        try {
+            setAuthenticationToContext(request);
+        } catch (ExpiredJwtException ee) {
+            request.setAttribute("exceptionCode", AuthExceptionCode.ACCESS_TOKEN_EXPIRED);
+        } catch (SignatureException se) {
+            request.setAttribute("exceptionCode", AuthExceptionCode.INVALID_SIGNATURE_ACCESS_TOKEN);
+        } catch (Exception e) {
+            log.warn("Exception : {}, message : {}", e.getClass(), e.getMessage());
+            request.setAttribute("exceptionCode", AuthExceptionCode.UNAUTHENTICATED);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return !checkAuthenticationExist(request) || !checkAuthenticationBearerExist(request);
+    }
+
+    private String getAuthenticationTokenToHeader(HttpServletRequest request) {
+        return request.getHeader(HttpHeaders.AUTHORIZATION);
+    }
+
+    private boolean checkAuthenticationExist(HttpServletRequest request) {
+        return getAuthenticationTokenToHeader(request) != null;
+    }
+    private boolean checkAuthenticationBearerExist(HttpServletRequest request) {
+        return getAuthenticationTokenToHeader(request).startsWith("Bearer ");
+    }
+
+    private void setAuthenticationToContext(HttpServletRequest request) {
+        Authentication authentication = createAuthenticatedToken(request);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private Authentication createAuthenticatedToken(HttpServletRequest request) {
+        Principal principal = createUserDetails(request);
+        return new UsernamePasswordAuthenticationToken(principal.getId(), null,
+                principal.getAuthorities());
+    }
+
+    private Principal createUserDetails(HttpServletRequest request) {
+        String token = getAuthenticationTokenToHeader(request).replace("Bearer ", "");
+        return new Principal(jwtProvider.getClaims(token));
+    }
+
+}
