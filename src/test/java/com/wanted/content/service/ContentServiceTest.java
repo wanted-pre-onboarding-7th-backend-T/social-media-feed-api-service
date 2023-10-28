9f77674666ebf1db6b0479bf46649095bef59c3b
Daeun Kim <jkde7721@naver.com>
2023-10-28 09:51:55 +0900

    test: 게시물 좋아요 Service 테스트 #3
    - 게시물 좋아요 성공 테스트
    - 게시물 좋아요 실패 테스트
    - 게시물 엔티티 조회 실패 테스트

---
+++ src/test/java/com/wanted/content/service/ContentServiceTest.java
@@ -0,0 +1,90 @@
+package com.wanted.content.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+import com.wanted.common.exception.CommonException;
+import com.wanted.content.entity.Content;
+import com.wanted.content.enums.SnsType;
+import com.wanted.content.repository.ContentRepository;
+import com.wanted.external.service.SnsApiService;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+
+@ExtendWith(MockitoExtension.class)
+class ContentServiceTest {
+
+    @InjectMocks
+    ContentService contentService;
+
+    @Mock
+    SnsApiService snsApiService;
+
+    @Mock
+    ContentRepository contentRepository;
+
+    @DisplayName("게시물 좋아요 테스트 : 성공")
+    @Test
+    void increaseLikeCountSuccess() {
+        //given
+        Long increasedLikeCount = 10L;
+        Content content = Content.builder().id(1L).contentSnsId("abc")
+            .type(SnsType.FACEBOOK).likeCount(5L).build();
+        given(contentRepository.findById(anyLong())).willReturn(Optional.of(content));
+        given(snsApiService.callLikeApi(anyString(), any(SnsType.class)))
+            .willReturn(Mono.just(increasedLikeCount));
+
+        //when
+        contentService.increaseLikeCount(content.getId());
+
+        //then
+        then(contentRepository).should(times(1)).findById(content.getId());
+        then(snsApiService).should(times(1))
+            .callLikeApi(content.getContentSnsId(), content.getType());
+        assertThat(content.getLikeCount()).isEqualTo(increasedLikeCount);
+    }
+
+    @DisplayName("게시물 좋아요 테스트 : 실패")
+    @Test
+    void increaseLikeCountFail() {
+        //given
+        Long originLikeCount = 5L;
+        Content content = Content.builder().id(1L).contentSnsId("abc")
+            .type(SnsType.FACEBOOK).likeCount(originLikeCount).build();
+        given(contentRepository.findById(anyLong())).willReturn(Optional.of(content));
+        given(snsApiService.callLikeApi(anyString(), any(SnsType.class)))
+            .willReturn(Mono.error(new RuntimeException()));
+
+        //when
+        contentService.increaseLikeCount(content.getId());
+
+        //then
+        then(contentRepository).should(times(1)).findById(content.getId());
+        then(snsApiService).should(times(1))
+            .callLikeApi(content.getContentSnsId(), content.getType());
+        assertThat(content.getLikeCount()).isEqualTo(originLikeCount);
+    }
+
+    @DisplayName("게시물 엔티티 조회 테스트 : 실패")
+    @Test
+    void getContentFail() {
+        //given
+        given(contentRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        //when, then
+        assertThatThrownBy(() -> contentService.getContent(1L))
+            .isInstanceOf(CommonException.class);
+    }
+}
