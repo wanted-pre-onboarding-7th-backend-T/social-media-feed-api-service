0a7da59e470e5607bf46f0ce029030a9778e261a
kho903 <gmldnr2222@naver.com>
2023-10-31 00:12:02 +0900

    feat: Content의 hashtags 필드에 FetchType.LAZY 추가 #7

--- src/main/java/com/wanted/content/entity/Content.java
+++ src/main/java/com/wanted/content/entity/Content.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -45,7 +46,7 @@ public class Content extends BaseTime {
     @Column(name = "content", columnDefinition = "TEXT", nullable = false)
     private String content;
 
-    @OneToMany(mappedBy = "content", orphanRemoval = true)
+    @OneToMany(mappedBy = "content", fetch = FetchType.LAZY)
     private List<ContentHashtag> hashtags = new ArrayList<>();
 
     @Column(name = "view_count", nullable = false)
